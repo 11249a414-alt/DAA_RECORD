90c809739c574b5345fbc7b78a07fd7b613ee677
11249a414-alt <11249a414@kanchiuniv.ac.in>
2026-03-06 15:52:24 +0530

    Add files via upload

---
+++ Kruskals_algorithm.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+int parent[10];
+
+int find(int i)
+{
+    while(parent[i])
+        i = parent[i];
+    return i;
+}
+
+int uni(int i, int j)
+{
+    if(i != j)
+    {
+        parent[j] = i;
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    int i, j, k, a, b, u, v, n, ne = 1;
+    int min, mincost = 0;
+    int cost[10][10];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter cost adjacency matrix:\n");
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= n; j++)
+        {
+            scanf("%d", &cost[i][j]);
+            if(cost[i][j] == 0)
+                cost[i][j] = 999;
+        }
+    }
+
+    printf("\nEdges of Minimum Spanning Tree:\n");
+
+    while(ne < n)
+    {
+        for(i = 1, min = 999; i <= n; i++)
+        {
+            for(j = 1; j <= n; j++)
+            {
+                if(cost[i][j] < min)
+                {
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
+            }
+        }
+
+        u = find(u);
+        v = find(v);
+
+        if(uni(u, v))
+        {
+            printf("%d edge (%d,%d) = %d\n", ne++, a, b, min);
+            mincost += min;
+        }
+
+        cost[a][b] = cost[b][a] = 999;
+    }
+
+    printf("\nMinimum cost = %d\n", mincost);
+
+    return 0;
+}
